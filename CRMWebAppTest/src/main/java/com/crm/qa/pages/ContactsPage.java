ef6f6d5f74b485ad73d90f4236ddc905e4385f4f
Sherine Thomas <sherinethachil@gmail.com>
2020-10-21 22:08:29 -0700

    Adding ContactsPage testcases

--- CRMWebAppTest/src/main/java/com/crm/qa/pages/ContactsPage.java
+++ CRMWebAppTest/src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -1,5 +1,26 @@
 package com.crm.qa.pages;
 
-public class ContactsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.crm.qa.base.TestBase;
+
+public class ContactsPage extends TestBase{
+	
+	@FindBy(xpath = "//button[text()='New']")
+	WebElement newBtn;
+	
+	// Initializing the Page Objects:
+	public ContactsPage() {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public AddNewContactsPage clickOnNewContacts(){
+		
+		newBtn.click();
+		return new AddNewContactsPage();
+		
+	}	
 }
--- CRMWebAppTest/src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ CRMWebAppTest/src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -3,47 +3,52 @@ package com.crm.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.AddNewContactsPage;
 import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.IndexPage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
 
-public class HomePageTest extends TestBase{
+public class ContactsPageTest extends TestBase{
 	
 	IndexPage indexPage;
 	LoginPage loginPage;
 	HomePage homePage;
 	ContactsPage contactsPage;
-
-	public HomePageTest() {
-		super();
-	}
-
+	AddNewContactsPage addNewContactsPage;
+		  
+	public ContactsPageTest(){
+			super();
+			
+	}	
+	
 	@BeforeMethod
-	public void setUp() {
+	public void setUp() throws InterruptedException {
+		
 		initialization();
-		
-		indexPage = new IndexPage();
-		loginPage = indexPage.clickOnLoginBtn();
+		indexPage=new IndexPage();
+		loginPage=indexPage.clickOnLoginBtn();
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		
+		contactsPage = homePage.clickOnContactsLink();
 	}
 	
 	@Test(priority=1)
-	public void verifyContactsLinkTest(){
-		contactsPage = homePage.clickOnContactsLink();
+	public void clickOnNewBtn(){
+		addNewContactsPage=contactsPage.clickOnNewContacts();
 		String actualUrl=driver.getCurrentUrl();
-		String expectedUrl="https://ui.freecrm.com/contacts";
+		String expectedUrl="https://ui.freecrm.com/contacts/new";
 		Assert.assertEquals(actualUrl, expectedUrl);
+
 	}	
-	
-	
+
 	@AfterMethod
 	public void tearDown(){
-		driver.quit();
+		//driver.quit();
 	}
 
 }
