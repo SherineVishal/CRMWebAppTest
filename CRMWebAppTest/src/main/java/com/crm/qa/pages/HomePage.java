fadc2e0a6c079a8281b35d5eabc83ff96237f9d9
Sherine Thomas <sherinethachil@gmail.com>
2020-10-21 21:40:22 -0700

    Adding HomePage testcases

--- CRMWebAppTest/src/main/java/com/crm/qa/pages/HomePage.java
+++ CRMWebAppTest/src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,5 +1,27 @@
 package com.crm.qa.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class HomePage extends TestBase{
+
+	@FindBy(xpath = "//span[text()='Contacts']")
+	WebElement contactsLink;
+
+	// Initializing the Page Objects:
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public ContactsPage clickOnContactsLink(){
+		contactsLink.click();
+		return new ContactsPage();
+	}
+
 
 }
+
+
---
+++ CRMWebAppTest/src/test/java/com/crm/qa/testcases/HomePageTest.java
@@ -0,0 +1,50 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.IndexPage;
+import com.crm.qa.pages.LoginPage;
+
+public class HomePageTest extends TestBase{
+	
+	IndexPage indexPage;
+	LoginPage loginPage;
+	HomePage homePage;
+	ContactsPage contactsPage;
+
+	public HomePageTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		
+		indexPage = new IndexPage();
+		loginPage = indexPage.clickOnLoginBtn();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		
+	}
+	
+	@Test(priority=1)
+	public void verifyContactsLinkTest(){
+		contactsPage = homePage.clickOnContactsLink();
+		String actualUrl=driver.getCurrentUrl();
+		String expectedUrl="https://ui.freecrm.com/contacts";
+		Assert.assertEquals(actualUrl, expectedUrl);
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+
+}
