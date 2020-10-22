3c997628a039b8e345486e8856e86f57f9b6f9e6
Sherine Thomas <sherinethachil@gmail.com>
2020-10-21 21:20:40 -0700

    Adding LoginPage testcases

--- CRMWebAppTest/src/main/java/com/crm/qa/pages/LoginPage.java
+++ CRMWebAppTest/src/main/java/com/crm/qa/pages/LoginPage.java
@@ -1,5 +1,36 @@
 package com.crm.qa.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	//Page Factory - OR:
+		@FindBy(name="email")
+		WebElement username;
+		
+		@FindBy(name="password")
+		WebElement password;
+		
+		@FindBy(xpath="//div[text()='Login']")
+		WebElement loginBtn;
+		
+				
+		//Initializing the Page Objects:
+		public LoginPage(){
+			PageFactory.initElements(driver, this);
+		}
+		
+		//Actions:
+			
+		public HomePage login(String usrname, String pwd){
+			username.sendKeys(usrname);
+			password.sendKeys(pwd);
+			loginBtn.click();
+			return new HomePage();
+		}
 
 }
---
+++ CRMWebAppTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,43 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.IndexPage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	
+	IndexPage indexPage;
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		indexPage = new IndexPage();	
+		loginPage=indexPage.clickOnLoginBtn();
+	}
+	
+	@Test(priority=3)
+	public void loginTest(){
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		String actualUrl=driver.getCurrentUrl();
+		String expectedUrl="https://ui.freecrm.com/";
+		Assert.assertEquals(actualUrl, expectedUrl);
+	}	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+
+}
